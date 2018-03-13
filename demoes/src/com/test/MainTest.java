e64c878f1dec3686f96abed2e5ec37c66aa715a9
gxtiantian <37324610+gxtiantian@users.noreply.github.com>
2018-03-13 15:50:56 +0800

    Update MainTest.java

--- demoes/src/com/test/MainTest.java
+++ demoes/src/com/test/MainTest.java
@@ -19,6 +19,7 @@ import com.entity.TrzPledgeApply;
 public class MainTest {
     public static void main(String[] args) throws Exception {
         String[] strs = {"123","ab","apple"};
+        System.out.println("for a remote update");
         System.out.println(Arrays.toString(strs));
     }
     
